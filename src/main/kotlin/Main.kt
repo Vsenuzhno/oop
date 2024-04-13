2b63cd59ccf3938fae3fbdfa871fe623cf5cf64d
Test <test@test.com>
2024-04-13 16:10:11 +0300

    Nullable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
 data class Post(
     val id: Int = 0,
-    val ownerId: Int = 0,
-    val fromId: Int = 0,
+    val ownerId: Int? = null,
+    val fromId: Int? = null,
     val createdBy: Int = 0,
     val authorName: String = "",
-    val text: String = "",
+    val text: String? = null,
     val replyOwnerId: Int = 0,
     val replyPostId: Int = 0,
     val published: Long = 0,
     val likes: Int = 0,
     val numbersOfComments: NumbersOfComments = NumbersOfComments()
 )
+
 
 data class NumbersOfComments(val numbersOfComments: Int = 0)
 
@@ -26,12 +27,19 @@ object WallService {
     fun update(newPost: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
             if (post.id == newPost.id) {
-                posts[index] = newPost.copy()
+                posts[index] = post.copy(
+                    ownerId = newPost.ownerId ?: post.ownerId,
+                    fromId = newPost.fromId ?: post.fromId,
+                    text = newPost.text ?: post.text,
+                    likes = newPost.likes,
+                    numbersOfComments = newPost.numbersOfComments.copy()
+                )
                 return true
             }
         }
         return false
     }
+
 
     fun printPosts() {
         for (post in posts) {
